dcf046d8a528a93939a14385a15a592477559797
BorisovaTatiana <devswiftos@gmail.com>
2026-02-23 22:42:42 +0300

    lesson_2 has done

---
+++ lesson_2.py
@@ -0,0 +1,126 @@
+
+# Задание 1
+a = 8
+b = 13
+x = 2.5
+y = -1.7
+print(a,b,x,y)
+print(type(a), type(b), type(x), type(y))
+
+# Задание 2
+
+num1 = 6
+num2 = 8
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+# Задание 3
+
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+
+a = -10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+
+a = 10
+b = -3
+print(a / b)
+print(a // b)
+print(a % b)
+
+# Задание 5
+
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+print(10 / 2 ** 2)
+
+# Задание 6
+
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+print(count)
+
+# Задание 7
+
+s1 = "Python"
+s2 = "Программирование"
+print(s1)
+print(s2)
+s3 = "Je comprends \n le langage \n de programmation Python"
+print(s3)
+s4 = ""
+print(len(s4))
+
+# Задание 8
+first_name = "Иван"
+last_name = "Иванов"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# Задание 9
+
+s = "Возраст: "
+age = 25
+print(s + str(age))
+
+# Задание 10
+
+# string1 = "xa "
+# string2 = string1 * 5
+# print(string2)
+# str_fl = float(string1)
+# string3 = string1 * 2.5
+# print(string3)
+# Не понимаю, как сделать... По мне так никак)
+
+# Задание 11
+
+text = "Привет, мир!"
+print(len(text))
+empty = ""
+print(len(empty))
+
+
+# Задание 12
+
+sentence = "Я изучаю Python"
+sent1 = "Python" in sentence
+print(sent1)
+sent2 = "Java" in sentence
+print(sent2)
+
+# Задание 13
+
+a = "apple"
+b = "banana"
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+# Задание 14
+
+
+code1 = ord('A')
+code2 = ord('a')
+code3 = ord('Я')
+print(code1)
+print(code2)
+print(code3)
+
